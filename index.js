33e3f38f90c05c3278244dabd7356e9797b15abf
Kiyan Gauss <fourcels@gmail.com>
2020-11-09 21:14:35 +0800

    下载

--- index.js
+++ index.js
@@ -1,31 +1,52 @@
 const fs = require('fs')
 const cheerio = require('cheerio');
+const path = require('path')
 
-const file = fs.readFileSync('./assets/稻田里的极简民宿.html')
-const $ = cheerio.load(file)
-
-// console.log($('img[data-src]').data('src'));
-
-// $('img[data-src]').each((i, item) => {
-//   console.log($(item).data('src'));
-// })
-let count = 1;
-let groupName = '稻田里的极简民宿'
-let outDir = './out'
-if (!fs.existsSync(outDir)) {
-  fs.mkdirSync(outDir, { recursive: true })
+function padding(num, length) {
+  let str = '' + num
+  if (str.length < length) {
+    str = '0'.repeat(length - str.length) + str
+  }
+  return str
 }
-const out = fs.createWriteStream(`${outDir}/稻田里的极简民宿.txt`)
-$('section,p').each((i, item) => {
-  if ($(item).find('span').text().startsWith('▼')) {
-    groupName = $(item).find('span').text().replace(/\s+/g, '').slice(1)
+
+function main() {
+  const args = process.argv.slice(2);
+  console.log(args);
+  if (args.length === 0) {
+    console.log('输入文件名');
+    return
   }
-  if ($(item).find('img').length > 0) {
-    const $img = $(item).find('img')
-    const src = $img.data('src')
-    const imgName = `${count}_${groupName}.${$img.data('type')}`
-    count++
-    out.write(`${src}\n\tout=${imgName}\n`)
+  const fileName = args[0]
+
+  const file = fs.readFileSync(fileName)
+  const $ = cheerio.load(file)
+
+  // console.log($('img[data-src]').data('src'));
+
+  // $('img[data-src]').each((i, item) => {
+  //   console.log($(item).data('src'));
+  // })
+  let count = 1;
+  let groupName = path.basename(fileName, '.html')
+  let outDir = './out'
+  if (!fs.existsSync(outDir)) {
+    fs.mkdirSync(outDir, { recursive: true })
   }
-})
-out.end()
+  const out = fs.createWriteStream(`${outDir}/${groupName}.txt`)
+  $('section,p').each((i, item) => {
+    if ($(item).find('span').text().startsWith('▼')) {
+      groupName = $(item).find('span').text().replace(/\s+/g, '').slice(1)
+    }
+    if ($(item).find('img').length > 0) {
+      const $img = $(item).find('img')
+      const src = $img.data('src')
+      const imgName = `${padding(count, 2)}_${groupName}.${$img.data('type')}`
+      count++
+      out.write(`${src}\n\tout=${imgName}\n`)
+    }
+  })
+  out.end()
+}
+
+main()
